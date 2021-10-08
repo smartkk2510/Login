d27df5f1a7fdd88033232b2d9d7619c50a3ec3a6
smartkk2510 <80613483+smartkk2510@users.noreply.github.com>
2021-10-08 16:18:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 //const bodyParser = require("body-parser");
-const request = require("request");
+//const request = require("request");
 require("dotenv").config();
 const app = express();
 app.use(express.static("public"));
